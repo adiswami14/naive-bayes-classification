4a466da40a2c43f46f615f4ebf0a9cd57425f4c7
adiswami14 <adithya1431@gmail.com>
2020-10-11 12:41:01 -0400

    Write tests for reading training labels

--- tests/test_main.cc
+++ tests/test_main.cc
@@ -1,2 +1,17 @@
 #define CATCH_CONFIG_MAIN
+#include <core/image.h>
 #include <catch2/catch.hpp>
+
+using std::vector;
+
+TEST_CASE("Training Labels Test") {
+  SECTION("Operator Overloading") {
+    std::ifstream myFile;
+    naivebayes::Image image;
+    myFile.open("mnistdatatraining/traininglabels");
+    while(myFile.good()) {
+      myFile >> image;
+    }
+    REQUIRE(image.GetTrainingLabelVec().size() == 5000);
+  }
+}
