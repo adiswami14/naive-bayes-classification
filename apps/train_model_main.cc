478e238e75880c3ee88b8a54045837bbf5d61f5c
adiswami14 <adithya1431@gmail.com>
2020-10-11 13:14:40 -0400

    Implement code for calculating prior possibilities

--- apps/train_model_main.cc
+++ apps/train_model_main.cc
@@ -1,13 +1,36 @@
+#include <core/image.h>
 #include <iostream>
-
-#include <core/rename_this_file.h>
+#include <fstream>
+#include <vector>
 
 // TODO: You may want to change main's signature to take in argc and argv
+using std::vector;
+
+vector<double> CalculatePriorProbabilities(std::ifstream &ifstream, naivebayes::Image& image) {
+  vector<double> prior_probabilities;
+  ifstream.open("mnistdatatraining/traininglabels");
+  while(ifstream.good()) {
+    ifstream>> image;
+  }
+
+  vector<int> training_vec = image.GetTrainingLabelVec();
+  for(size_t num = 0; num <= 9; num++) {
+    //the number of times a certain number shows up in training labels over total number of training labels
+    double probability_of_num =
+        static_cast<double>(std::count(training_vec.begin(), training_vec.end(), num))/training_vec.size();
+
+    prior_probabilities.push_back(probability_of_num);
+  }
+  return prior_probabilities;
+}
+
 int main() {
   // TODO: Replace this with code that reads the training data, trains a model,
   // and saves the trained model to a file.
 
-  std::cout << "Welcome to " << naivebayes::Placeholder().GetBestClass()
-            << std::endl;
+  std::ifstream ifstream;
+  naivebayes::Image image;
+  vector<double> prior_probabilities = CalculatePriorProbabilities(ifstream, image);
+
   return 0;
 }
