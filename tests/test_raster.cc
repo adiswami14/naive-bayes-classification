791cdf95a1d19cbcc734172730f1f500108e376c
adiswami14 <adithya1431@gmail.com>
2020-10-13 02:50:20 -0400

    Add test file for Raster class

---
+++ tests/test_raster.cc
@@ -0,0 +1,22 @@
+#include <core/raster.h>
+#include <catch2/catch.hpp>
+
+using std::vector;
+
+TEST_CASE("Setters & Getters"){
+  SECTION("Set Raster Vector") {
+    naivebayes::Raster raster;
+    REQUIRE_THROWS_AS(raster.SetRasterVector(vector<vector<char>>()), std::invalid_argument);
+    vector<char> vec (3, 0);
+    vector<vector<char>> vec_2d;
+    vec_2d.push_back(vec);
+    raster.SetRasterVector(vec_2d);
+    REQUIRE(vec_2d == raster.GetRasterVector());
+  }
+
+  SECTION("Set Raster Image Class"){
+    naivebayes::Raster raster;
+    raster.SetRasterImageClass(4);
+    REQUIRE(raster.GetRasterImageClass() == 4);
+  }
+}
